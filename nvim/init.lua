dd22a64b01710414f358c19d913ff4547591859b
Joe Albanese <joe-albanese@protonmail.com>
2025-03-13 16:49:23 -0400

    add bash lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,16 @@ lspconfig.clangd.setup({
   },
 })
 
-vim.cmd('colorscheme pablo')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'sh',
+  callback = function()
+    vim.lsp.start({
+      name = 'bash-language-server',
+      cmd = { 'bash-language-server', 'start' },
+    })
+  end,
+})
+
+vim.cmd('colorscheme murphy')
 vim.cmd('set shiftwidth=4 smarttab')
 vim.cmd('set number')
